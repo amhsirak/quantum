ae2996a2e16397f22e36f3190a4898300428af79
karishmas6 <carishmashukla28@gmail.com>
2021-06-26 23:08:37 +0530

    load images

--- aos/js/scenes/sceneLoad.js
+++ aos/js/scenes/sceneLoad.js
@@ -4,7 +4,7 @@ class SceneLoad extends Phaser.Scene {
     }
     preload() {
 
-         this.bar=new Bar({scene:this,x:game.config.width/2,y:game.config.height/2});
+        this.bar=new Bar({scene:this,x:game.config.width/2,y:game.config.height/2});
     	this.progText=this.add.text(game.config.width/2,game.config.height/2,"0%",{color:'#ffffff',fontSize:game.config.width/20});
     	this.progText.setOrigin(0.5,0.5);
     	this.load.on('progress', this.onProgress, this);
@@ -12,14 +12,14 @@ class SceneLoad extends Phaser.Scene {
         this.load.image("button1","images/ui/buttons/2/1.png");
         this.load.image("button2","images/ui/buttons/2/5.png");
 
-      // this.load.audio('cat',["audio/meow.mp3","audio/meow.ogg"]);
-       // this.load.audio('backgroundMusic',["audio/background.mp3","audio/background.ogg"]);
-
         this.load.image("toggleBack","images/ui/toggles/1.png");
         this.load.image("sfxOff","images/ui/icons/sfx_off.png");
         this.load.image("sfxOn","images/ui/icons/sfx_on.png");
         this.load.image("musicOn","images/ui/icons/music_on.png");
         this.load.image("musicOff","images/ui/icons/music_off.png");
+
+        this.load.image("ship","images/player.png");
+        this.load.image("background","images/background.jpg");
     }
     onProgress(value)
     {
