1b782a6dd5ae81b7a8875bfc6adc9b642c4a68c9
karishmas6 <carishmashukla28@gmail.com>
2021-06-27 00:06:11 +0530

    ship

--- aos/js/scenes/sceneMain.js
+++ aos/js/scenes/sceneMain.js
@@ -19,7 +19,15 @@ class SceneMain extends Phaser.Scene {
         this.background = this.add.image(0,0,'background');
         this.background.setOrigin(0,0);
         this.ship = this.physics.add.sprite(this.centerX,this.centerY,'ship');
+        // make the ship move
+        this.background.setInteractive();
+        this.background.on('pointerdown', this.backgroundClicked, this);
+    }
+    backgroundClicked() {
+        var targetX = this.background.input.localX;
+        var targetY = this.background.input.localY;
 
+        this.physics.moveTo(this.ship, targetX, targetY, 20);
     }
     
     update() {
