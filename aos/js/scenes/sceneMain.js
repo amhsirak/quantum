30aa6dbe8aabe5c5fae7403f33e4e93b3d0ef56e
karishmas6 <carishmashukla28@gmail.com>
2021-06-26 23:56:03 +0530

    ship

--- aos/js/scenes/sceneMain.js
+++ aos/js/scenes/sceneMain.js
@@ -16,6 +16,8 @@ class SceneMain extends Phaser.Scene {
         this.centerX = game.config.width/2;
         this.centerY = game.config.height/2;
 
+        this.background = this.add.image(0,0,'background');
+        this.background.setOrigin(0,0);
         this.ship = this.physics.add.sprite(this.centerX,this.centerY,'ship');
 
     }
