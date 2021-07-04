94dd920282842f68b5eaf1eb4a3b209e66245588
karishmas6 <carishmashukla28@gmail.com>
2021-07-04 16:54:59 +0530

    about

---
+++ quantum/js/scenes/sceneAbout.js
@@ -0,0 +1,11 @@
+class SceneAbout extends Phaser.Scene {
+    constructor() {
+        super('SceneMain');
+    }
+    preload() {
+       
+    }
+    create() {
+    }
+    update() {}
+}
--- quantum/js/scenes/sceneTitle.js
+++ quantum/js/scenes/sceneTitle.js
@@ -46,9 +46,6 @@ class SceneTitle extends Phaser.Scene {
         emitter.on('start_game',this.startGame,this);
         emitter.on('about_game',this.aboutGame,this);
 
-        // let sb=new SoundButtons({
-        //     scene:this
-        // });
     }
     startGame()
     {
