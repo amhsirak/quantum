123848a2fd03dbd8666fbb5b99583c408c086b6b
karishmas6 <carishmashukla28@gmail.com>
2021-07-12 23:24:19 +0530

    small changes

--- quantum/toadd.js
+++
@@ -1,9 +0,0 @@
-# In scene A
-this.scene.launch('sceneB')
-this.scene.pause();
-
-# Then in sceneB, you can return to sceneA:
-button.on('pointerdown', function() {
-    this.scene.resume('sceneA');
-    this.scene.stop();
-})
